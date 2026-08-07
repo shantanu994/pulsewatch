ea1549a06ffbe1d79bff830afe6465580c1c5507
shantanu994 <shantanukulkarni994@gmail.com>
2026-08-07 20:22:32 +0530

    Wrote a smiple code in  Database file

---
+++ app/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
