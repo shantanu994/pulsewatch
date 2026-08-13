c9a8e16f155ca56b266bc1ce1a6323ea06d0db6d
shantanu994 <shantanukulkarni994@gmail.com>
2026-08-13 23:12:15 +0530

    Updated schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -11,3 +11,15 @@ class UserOut(BaseModel):
     class Config:
         from_attributes = True
 
+class MonitorCreate(BaseModel):
+    url: str
+    interval_seconds: int = 300
+
+class MonitorOut(BaseModel):
+    id: int
+    url: str
+    interval_seconds: int
+    is_active: bool
+
+    class Config:
+        from_attributes = True
