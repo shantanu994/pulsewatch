7d39fa9487d6f96d5a66cb339bcc36ad25fbd4fd
shantanu994 <shantanukulkarni994@gmail.com>
2026-09-13 01:02:13 +0530

    Fix

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": "require"})
 
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
